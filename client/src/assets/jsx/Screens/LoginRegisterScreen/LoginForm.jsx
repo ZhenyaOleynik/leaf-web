bb21c7b48c39b686bda65a64f87b3fbc1c0792f6
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-11-30 00:14:55 +0200

    Handlers added, input type submit replaced with button

--- client/src/assets/jsx/Screens/LoginRegisterScreen/LoginForm.jsx
+++ client/src/assets/jsx/Screens/LoginRegisterScreen/LoginForm.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
 import '../../../css/login-register-form.css'
+import Axios from 'axios'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 
 const LoginForm = () => {
+
+    Axios.defaults.withCredentials = true
+
+    const handleLogin = async (e) => {
+
+
+        const login = document.getElementById('login').value
+        const password = document.getElementById('password').value
+
+        console.log(login)
+        console.log(password)
+
+        await Axios.post('http://localhost:5000/login', { login, password })
+            .then(res => console.log(res.data))
+            .catch(err => console.log('post error: ' + err))
+
+        Axios.get('http://localhost:5000/auth')
+            .then(res => console.log(res.data))
+            .catch(err => console.log('get error: ' + err))
+
+    }
+
     return (
-        <React.Fragment>
-            <label htmlFor={"username"}>USERNAME</label>
-            <input type="text" id="username" />
+        <>
+            <label htmlFor={"login"}>LOGIN</label>
+            <input type="text" id="login" />
             <label htmlFor={"password"}>PASSWORD</label>
             <input type="password" id="password" />
-            <input type="submit" value="submit" className="submit" />
-        </React.Fragment>
+            <button type="button" className="submit" onClick={handleLogin}>Submit</button>
+        </>
     )
 }
 
