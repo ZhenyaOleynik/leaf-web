facdcb7dbe1eb65e56c22f6e4f8771f9a36f9b47
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-12-02 00:52:51 +0200

    Navbar changed for different users

--- client/src/assets/jsx/Components/Navbar.jsx
+++ client/src/assets/jsx/Components/Navbar.jsx
@@ -3,28 +3,44 @@ import '../../css/navbar.css'
 import { Link } from 'react-router-dom'
 import { Menu } from 'antd'
 import 'antd/dist/antd.css'
+import Axios from 'axios'
+import { useEffect } from 'react';
 
-
-const styles = {
-    backgroundColor: '#1b283b',
-    alignContent: 'right'
-}
+Axios.defaults.withCredentials = true
 
 const NavBar = () => {
 
-    let [current, setCurrent] = useState('profile')
+    const [isAuth, setIsAuth] = useState(false)
+    const [current, setCurrent] = useState('profile')
+
+    useEffect(() => {
+        Axios.get('http://localhost:5000/api/auth/isAuth')
+            .then(res => setIsAuth(res.data.auth))
+            .catch(err => false)
+    }, [])
+
+    useEffect(() => setCurrent(isAuth ? 'profile' : 'login/register'), [isAuth])
 
     const handleClick = e => setCurrent(e.key)
 
     return (
         <>
             <Menu onClick={handleClick} selectedKeys={current} mode='horizontal' theme='dark'>
-                <Menu.Item key='profile'>
-                    {/* <Link to='/'>Your profile</Link> */}
-                    <a href="#">Your profile</a>
-                </Menu.Item>
+                {
+                    isAuth ?
+                        <>
+                            <Menu.Item key='profile'>
+                                <a href="#">Your profile</a>
+                            </Menu.Item>
+                        </>
+                        :
+                        <>
+                            <Menu.Item key='login/register'>
+                                <a href="#">Login/Register</a>
+                            </Menu.Item>
+                        </>
+                }
                 <Menu.Item key='all_users'>
-                    {/* <Link to='/'>All Users</Link> */}
                     <a href="#">All Users</a>
                 </Menu.Item>
             </Menu>
