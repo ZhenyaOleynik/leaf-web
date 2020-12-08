7df128c56396d81b1ded4f4077a993670fd4a0a1
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-12-09 01:00:16 +0200

    Routing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React from "react"
 import './App.css'
+import AllUsersPage from './assets/jsx/Pages/AllUsersPage'
+import { BrowserRouter, Route } from "react-router-dom"
+import LoginRegisterPage from "./assets/jsx/Pages/LoginRegisterPage"
+import Profile from "./assets/jsx/Pages/Profile"
 
-function App() {
+export default function App() {
   return (
-      <></>
+    <BrowserRouter>
+      <Route exact path='/' component={AllUsersPage} />
+      <Route exact path='/table' component={AllUsersPage} />
+      <Route path='/profile' component={Profile} />
+      <Route path='/auth' component={LoginRegisterPage} />
+    </BrowserRouter>
   )
 }
 
-export default App;
