3d0737fc4edcbcaae058335775bb9121edd5aa8c
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-12-10 01:12:35 +0200

    Profile start using UserCard with data from server

--- client/src/assets/jsx/Pages/Profile.jsx
+++ client/src/assets/jsx/Pages/Profile.jsx
@@ -1,16 +1,42 @@
+import Axios from 'axios'
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 import NavBar from '../Components/Navbar'
 import UserCard from '../Components/UserCard'
 
 const Profile = () => {
+
+    const [userInfo, setUserInfo] = useState({})
+    // const [mounted, setMounted] = useState(false)
+
+    // console.log(1)
+
+    // if (!mounted) {
+
+    // }
+
+    // useEffect(() => setMounted(true), [])
+
+    useEffect(() => {
+        console.log(2)
+        Axios.get('http://localhost:5000/api/users/getCurrentUser')
+            .then(res => {
+                setUserInfo(res.data[0])
+                console.log(res.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+
     return (
         <>
             <NavBar page='profile' />
             <UserCard userInfo={{
-                avatar: 'E:/programming/Java/SecondYear/SnakeGame/src/resources/apple.png',
-                login: 'fop@t@',
-                first_name: 'Fopt',
-                last_name: 'FuPT'
+                avatar: userInfo.photo,
+                login: userInfo.login,
+                first_name: userInfo.first_name,
+                last_name: userInfo.last_name
             }} />
         </>
     )
