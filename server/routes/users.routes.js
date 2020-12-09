be30ed743ab93fd659bf2e876fa2500ed7a25f27
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-12-10 01:21:13 +0200

    C:/Program Files/Git/delete route changed to prevent delete yourself & /getCurrentUser & /logout routes added

---
+++ server/routes/users.routes.js
@@ -0,0 +1,104 @@
+const { Router } = require('express')
+const dotenv = require('dotenv')
+const router = Router()
+
+const db = require('../db')
+
+dotenv.config({ path: './.env' })
+
+router.get('/getAll', (req, res) => {
+
+    const query = 'SELECT * FROM `users`'
+
+    db.query(query, (err, result, fields) => {
+        if (!err) {
+            res.status(200).json(result)
+        }
+        else {
+            res.status(500).json({ query: query, error: err })
+        }
+    })
+
+})
+
+router.get('/getCurrentUser', (req, res) => {
+
+    const id = req.session.userId
+
+    const query_select_data = 'SELECT * FROM `users` WHERE `id` = ?'
+    const query_select_role = 'SELECT * FROM `roles` WHERE `id` = ?'
+
+    db.query(query_select_data, [id], (err, result) => {
+        if (err) {
+            res.status(500).json({ err })
+        }
+        else {
+            if (result.length === 0) {
+                res.status(200).json(result)
+            }
+            else {
+                db.query(query_select_role, [id], (err_final, result_final) => {
+                    if (err_final) {
+                        res.status(500).json({ err_final })
+                    }
+                    else {
+                        result[0].title = result_final[0].title
+                        res.status(200).json(result)
+                    }
+                })
+            }
+        }
+    })
+
+
+})
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            res.status(500).json({ err })
+        }
+        else {
+            res.clearCookie(process.env.SESSION_NAME)
+            res.status(200).json({ msg: 'logout...' })
+        }
+    })
+})
+
+router.delete('/delete' + '/:id', (req, res) => {
+
+    const id = req.params.id
+
+    if (id == req.session.userId)
+        return res.status(200).json({ msg: 'cannot delete yourself' })
+
+    db.query('DELETE FROM `users` WHERE `id` = ?', [id], (err, result) => {
+        if (err) {
+            console.log('sql delete err: ' + err)
+            res.send(500).json({ err })
+        }
+        res.status(200)
+    })
+})
+
+router.put('/update' + '/:id', (req, res) => {
+
+    const id = req.params.id
+    const data = req.body.data
+
+    data[data.length] = id
+
+    console.log(1)
+
+    const query = 'UPDATE `users` SET `first_name`=? `last_name`=? `login`=? `password`=? `photo`=? WHERE `id`=?'
+
+    db.query(query, data, (err, result) => {
+        if (err) {
+            console.log('sql delete err: ' + err)
+            res.send(500).json({ msg: err })
+        }
+        res.status(200)
+    })
+})
+
+module.exports = router
