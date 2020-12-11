2e24933b5739d603c5edae71405cd8c42a610529
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-12-11 03:19:08 +0200

    '/edit' route added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import AllUsersPage from './assets/jsx/Pages/AllUsersPage'
 import { BrowserRouter, Route } from "react-router-dom"
 import LoginRegisterPage from "./assets/jsx/Pages/LoginRegisterPage"
 import Profile from "./assets/jsx/Pages/Profile"
+import EditForm from "./assets/jsx/Components/EditForm"
+import EditPage from "./assets/jsx/Pages/EditPage"
 
 export default function App() {
   return (
@@ -12,6 +14,7 @@ export default function App() {
       <Route exact path='/table' component={AllUsersPage} />
       <Route path='/profile' component={Profile} />
       <Route path='/auth' component={LoginRegisterPage} />
+      <Route path='/edit' component={EditPage} />
     </BrowserRouter>
   )
 }
